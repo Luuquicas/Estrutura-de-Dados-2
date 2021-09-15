8821b5b2305b4d35dee845ee17f2d0a63a70f83a
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-09-15 00:02:08 -0300

    create atv01

---
+++ Lista 1 - Recursividade/ex01/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+unsigned long long int sumValues(unsigned long long int n){
+    int i=0;
+    if(n == 1)
+        return 1;
+    else
+        return 
+}
+
+int main(){
+
+
+    return 0;
+}
