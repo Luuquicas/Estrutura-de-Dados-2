1718c152155a7190a82457de1be1b506c51be31f
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-09-17 10:10:47 -0300

    update heap

--- Heap Binária/Heap.c
+++ Heap Binária/Heap.c
@@ -4,11 +4,6 @@
 #define MAX 10
 // 16, 14, 10, 8, 7, 9, 3, 2, 4, 1
 
-int Heap_size(heap* a){
-    size_t tam = sizeof(a)/sizeof(a->data[0]);
-    return (int) tam;
-}
-
 int Parent(int i){
     return i/2;
 }
@@ -21,26 +16,27 @@ int Right(int i){
     return (2*i)+1;
 }
 
-void Max_heapfy(heap* a, int i){
+void Max_heapfy(int* a, int i){
     int l = Left(i);
     int r = Right(i);
     int largest, aux;
 
-    if(l <= Heap_size(a) && a->data[l] > a->data[i])
+    if(l <= Heap_size(a) && a[l] > a[i])
         largest = l;
     else
         largest = i;
 
-    if(r <= Heap_size(a) && a->data[r] > a->data[largest])
+    if(r <= Heap_size(a) && a[r] > a[largest])
         largest = r;
     
     if(largest != i)
-        aux = a->data[i];
-        a->data[i] = a->data[largest];
-        a->data[largest] = aux;
+        aux = a[i];
+        a[i] = a[largest];
+        a[largest] = aux;
         Max_heapfy(a, largest);
 }
 
 int main(){
+    int a[] = {2,8,4,1,7,14,16,10,9,3};
     
 }
--- Heap Binária/Heap.h
+++ Heap Binária/Heap.h
@@ -2,14 +2,10 @@
 #define HEAP_H
 #define MAX 10
 
-typedef struct no{
-    int data[MAX];
-}heap;
-
 int Parent(int);
 int Right(int);
 int Left(int);
-int Heap_size(heap*);
-void Max_heapfy(heap*, int);
+int Heap_size(int*);
+void Max_heapfy(int*, int);
 
 #endif
