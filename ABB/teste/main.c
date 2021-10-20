44b5acf3686d5ad2397a7d7362a660969884f1b7
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-10-19 21:14:52 -0300

    make tests for exercise

---
+++ ABB/teste/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char input[] = "(12(5()(6))(10()(1)))";
+    char *pt;
+    int i=0;
+    char v[10];
+
+    pt = strtok(input, "()");
+    while(pt){
+        printf("Token: %s\n", pt);
+        strcpy(&v[i], pt);
+        i++;
+        pt = strtok(NULL, "()");
+    }
+    for(int j=0; j<5; j++){
+        printf("v[%i] = %c\n", i, v[i]);
+    }
+    return 0;
+}
