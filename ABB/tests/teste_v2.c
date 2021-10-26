ac211e5867426028433311cc767c47c6bae9563d
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-10-25 22:26:38 -0300

    finally, i found the mistery

--- ABB/tests/teste_v2.c
+++ ABB/tests/teste_v2.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 int main(){
-    char s[] = "(7(5()(6))(10()(12)))";
-    int len = strlen(s);
-    int v = 0;
+
+    char palavra[] = "(7(5()(6))(10()(12)))";
+    int len = strlen(palavra);
+    char *pt;
+    int v[10];
+    char v1[len][len];
     int i = 0;
 
+    pt = strtok(palavra, "()");
+    while(pt){
+        strcpy(v1[i], pt);
+        pt = strtok(NULL, "()");
+        i++;
+    }
+
+    for(i=0; i<5; i++){
+        v[i] = atoi(v1[i]);
+    }
+
+    for(int i = 0; i<5; i++){
+        printf("num %i: %i\n", i, v[i]);
+    }
     return 0;
 }
